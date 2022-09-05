78058f8f7ef59120290b6e44e2e23f991fae2182
Mandala Trading <mandalareports@gmail.com>
2022-09-05 19:12:27 +0530

    CU-2t2utbb - Adding dynamic page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = t("page_title")
+
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} « #{base_title}"
+    end
+  end
 end
